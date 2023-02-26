00e299f74f3ece3ea87f19ad20961f9116d98f3a
merdan2000 <merdan2000@icloud.com>
2023-02-26 13:28:44 +0300

    fix signup

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import s from "../SignIn/signin.module.css";
+import s from "./signup.module.css";
 import {Link} from "react-router-dom";
 
 function SignUp(): JSX.Element {
